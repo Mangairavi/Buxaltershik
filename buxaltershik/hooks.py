a667b76fb786de6a7b0768749afb5f92dd6cf4f4
Mangairavi <mangai23ravi@gmail.com>
2025-08-30 08:32:30 +0000

    Initial commit - buxaltershik app

--- buxaltershik/hooks.py
+++ buxaltershik/hooks.py
@@ -8,6 +8,14 @@ app_license = "mit"
 # Apps
 # ------------------
 
+fixtures = [
+    "Custom Field",
+    "Property Setter",
+    "Print Format",
+    "Report",
+    "Workflow"
+]
+
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
